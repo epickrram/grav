31ae5fa05d1211cbc4d398e0f10f6119d780a740
Mark Price <markprice@improbable.io>
2017-04-15 14:09:00 +0100

    add usurpers by pid

--- src/cpu/bcc_scheduler_profile.py
+++ src/cpu/bcc_scheduler_profile.py
@@ -21,11 +21,35 @@ struct scheduled_out_state_t {
     int unknown;
 };
 
+struct proc_name_t {
+    char comm[TASK_COMM_LEN];
+};
+
+struct proc_counter_t {
+    int count;
+//    char *proc_name[TASK_COMM_LEN];
+};
+
+BPF_TABLE("hash", pid_t, struct proc_counter_t, usurpers, 1024);
 BPF_TABLE("hash", pid_t, struct scheduled_out_state_t, scheduled_out_states, 1024);
 
 int trace_finish_task_switch(struct pt_regs *ctx, struct task_struct *prev) {
 
     pid_t prev_pid = prev->pid;
+    pid_t parent_pid = prev->parent->pid;
+    pid_t incoming_pid = bpf_get_current_pid_tgid();
+    
+    struct proc_name_t pname = {};
+    bpf_get_current_comm(&pname.comm, sizeof(pname.comm));
+
+    struct proc_counter_t *counter = usurpers.lookup(&incoming_pid);
+    if (counter == 0) {
+        // TODO - strcopy (need to make child pid to proc name)?
+        struct proc_counter_t new_counter = {/*.proc_name = &pname.comm, */.count = 0};
+        counter = &new_counter;
+        usurpers.update(&incoming_pid, counter);
+    }
+    counter->count++;
 
     struct scheduled_out_state_t *states = scheduled_out_states.lookup(&prev_pid);
     if (states == 0) {
@@ -53,6 +77,10 @@ b.attach_kprobe(event="finish_task_switch", fn_name="trace_finish_task_switch")
 
 time.sleep(int(sys.argv[1]))
 results = dict()
+
+for k, v in b["usurpers"].iteritems():
+    print str(k) + " -> " + str(v.count)
+
 for k,v in b["scheduled_out_states"].iteritems():
     tid_stats = dict()
     tid_stats['R'] = v.running
