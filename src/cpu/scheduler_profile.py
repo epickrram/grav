e5c43b325d7ab90971ce3a49ebb1b9867f44697f
Mark Price <markprice@improbable.io>
2017-04-20 17:21:29 +0100

    display horizontal

--- src/cpu/scheduler_profile.py
+++ src/cpu/scheduler_profile.py
@@ -30,13 +30,14 @@ def get_stroke(state):
     return STROKE_COLOURS[state]
 
 
-def write_cell(writer, x_offset, y_offset, width, height, state, thread_name, count, total):
+def write_cell(writer, x_offset, y_offset, width, height, state, thread_name, count, total, text_written):
     state_percentage = 100 * (count / float(total))
     cell_text = '{}/{} ({:.2f}%)'.format(thread_name, state, state_percentage)
     writer.write('<g><title>{}</title>'.format(cell_text))
     writer.write('<rect x="{}" y="{}" width="{}" height="{}" style="fill: {}; stroke:{}">'.format(x_offset, y_offset, width, height, get_fill(state), get_stroke(state)))
     writer.write('</rect>\n')
-    writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" style="text-overflow: clip" fill="#000">{}</text>'.format(x_offset, y_offset + 12, width, cell_text))
+    if not text_written:
+        writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" fill="#000">{}</text>'.format(x_offset, y_offset + 12, width, cell_text))
     writer.write('</g>\n')
 
 
@@ -44,24 +45,23 @@ def write_svg(width, height, thread_scheduling, max_total, tid_to_thread_name, p
     writer = open('scheduler-profile-{}.svg'.format(process_id), 'w')
     write_svg_header(writer, width, height)
 
-    column_width = float(width / len(thread_scheduling))
-    single_sample_height = float(height / float(max_total))
+    row_height = float(height / len(thread_scheduling))
 
-    x_offset = 0
+    y_offset = 0
     for tid in sorted(thread_scheduling.iterkeys()):
-        y_offset = height
+        x_offset = 0
         tid_sample_count = thread_scheduling[tid]['total']
-        if tid_sample_count > 0:
-            single_sample_height = float(height / float(thread_scheduling[tid]['total']))
-            for state in ['S', 'R', 'D', 'U']:
-                sample_count = thread_scheduling[tid][state]
-                if sample_count > 0:
-                    state_height = sample_count * single_sample_height
-                    write_cell(writer, x_offset, y_offset - state_height, column_width, state_height, state, tid_to_thread_name[tid], sample_count, tid_sample_count)
-                    y_offset -= state_height
-            x_offset += column_width
-        else:
-            print "{}: {}".format(tid, thread_scheduling[tid])
+        single_sample_width = float(width / float(thread_scheduling[tid]['total']))
+        text_written = False
+        for state in ['S', 'R', 'D', 'U']:
+            sample_count = thread_scheduling[tid][state]
+
+            if sample_count > 0:
+                state_width = sample_count * single_sample_width
+                write_cell(writer, x_offset, y_offset, state_width, row_height, state, tid_to_thread_name[tid], sample_count, tid_sample_count, text_written)
+                x_offset += state_width
+                text_written = True
+        y_offset += row_height
 
     write_svg_footer(writer)
     writer.close()
@@ -86,7 +86,7 @@ def filter_scheduler_info(thread_scheduling_info, threads_to_include):
     filtered = dict()
     for k in thread_scheduling_info.iterkeys():
         if k in threads_to_include.keys():
-            if thread_scheduling_info[k]["S"] != thread_scheduling_info[k]["total"]:
+            if thread_scheduling_info[k]["S"] != thread_scheduling_info[k]["total"] and thread_scheduling_info[k]["total"] != 0:
                 filtered[k] = thread_scheduling_info[k]
                 if filtered[k]['total'] > max_total_value:
                     max_total_value = filtered[k]['total']
