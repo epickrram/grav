79238c071753227f9b02b608b415497220cc981b
Mark Price <markprice@improbable.io>
2017-04-22 05:36:17 +0100

    horizontal layout

--- src/cpu/cpu_tenancy.py
+++ src/cpu/cpu_tenancy.py
@@ -3,8 +3,6 @@
 import re
 import sys
 
-# TODO apply text-overflow: ellipsis to svg text element, plus limit width?
-
 CPU_COLOURS = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 #perf script -F comm,pid,tid,cpu,time,event
 
@@ -62,41 +60,53 @@ def get_fill(cpu_id):
     # TODO support 256 cpus
     return CPU_COLOURS[cpu_id % len(CPU_COLOURS)]
 
-def write_cell(writer, x_offset, y_offset, width, height, cpu_id, tid):
+def write_cell(writer, x_offset, y_offset, width, height, cpu_id, tid, write_thread_name):
     thread_name = str(tid)
     cell_text = '{}/CPU{}'.format(thread_name, cpu_id)
     writer.write('<g><title>{}</title>'.format(cell_text))
     writer.write('<rect x="{}" y="{}" width="{}" height="{}" fill="{}">'.format(x_offset, y_offset, width, height, get_fill(cpu_id)))
     writer.write('</rect>\n')
-    writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" style="text-overflow: clip" fill="#000">{}</text>'.format(x_offset, y_offset + 12, width, cell_text))
+    if write_thread_name:
+        writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" style="text-overflow: clip" fill="#000">{}</text>'.format(x_offset, y_offset + 12, width, thread_name))
+    writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" style="text-overflow: clip" fill="#000">{}</text>'.format(x_offset, y_offset + 22, width, "CPU{}".format(cpu_id)))
     writer.write('</g>\n')
 
-def write_svg(width, height, cpu_tenancy_by_pid, max_sample_count, tid_to_thread_name):
-    writer = open('test.svg', 'w')
+def write_svg(width, height, cpu_tenancy_by_pid, max_sample_count, tid_to_thread_name, process_id):
+    writer = open('cpu-tenancy-{}.svg'.format(process_id), 'w')
     write_svg_header(writer, width, height)
 
     column_width = float(width / calculate_number_of_columns(cpu_tenancy_by_pid))
     single_sample_height = float(height / float(max_sample_count))
+    row_height = float((height - 60) / calculate_number_of_columns(cpu_tenancy_by_pid))
+    unit_width = float((width - 40) / float(max_sample_count))
 
     x_offset = 0
-    y_offset = height
+    y_offset = 50
     thread_name_to_tid = dict()
     for t in tid_to_thread_name:
         thread_name_to_tid[tid_to_thread_name[t]] = t
     for pid in sorted(cpu_tenancy_by_pid.iterkeys()):
         for tid in sorted(cpu_tenancy_by_pid[pid].iterkeys()):
             cpu_sample_count = cpu_tenancy_by_pid[pid][tid]
-            y_offset = height
+            # y_offset = height
+            x_offset = 20
+            write_thread_name = True
             for cpu_id in sorted(cpu_sample_count.iterkeys()):
                 if cpu_id is not 'all':
-                    sample_height = single_sample_height * cpu_sample_count[cpu_id]
+                    #sample_height = single_sample_height * cpu_sample_count[cpu_id]
+                    sample_width = unit_width * cpu_sample_count[cpu_id]
                     thread_name = "unknown"
                     if tid in tid_to_thread_name:
                         thread_name = tid_to_thread_name[tid]
-                    write_cell(writer, x_offset, y_offset - sample_height, column_width, sample_height, cpu_id, thread_name)
-                    y_offset -= sample_height
+                    # TODO add percentage to cell text
+                    # TODO write column zero last (or set z-index?)
+                    write_cell(writer, x_offset, y_offset, sample_width, row_height, cpu_id, thread_name, write_thread_name)
+                    write_thread_name = False
+                    #y_offset -= sample_height
+                    x_offset += sample_width
 
-            x_offset += column_width
+            #x_offset += column_width
+            y_offset += row_height
                 
     write_svg_footer(writer)
     writer.close()
@@ -116,7 +126,8 @@ def get_tid_to_thread_name(jstack_file):
 
 
 if __name__ == "__main__":
-    tid_to_thread_name = get_tid_to_thread_name(sys.argv[1])
+    process_id = sys.argv[1]
+    tid_to_thread_name = get_tid_to_thread_name(sys.argv[2])
     cpu_tenancy_by_pid, max_sample_count = get_cpu_tenancy_count_by_tid()
 
-    write_svg(1200, 600, cpu_tenancy_by_pid, max_sample_count, tid_to_thread_name)
+    write_svg(1200, 660, cpu_tenancy_by_pid, max_sample_count, tid_to_thread_name, process_id)
