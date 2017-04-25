5a1475191797f6f555bcad66f6598c1e97a923a0
Mark Price <markprice@improbable.io>
2017-04-25 08:47:10 +0100

    colours

--- src/cpu/cpu_tenancy.py
+++ src/cpu/cpu_tenancy.py
@@ -6,6 +6,33 @@ import sys
 CPU_COLOURS = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 #perf script -F comm,pid,tid,cpu,time,event
 
+def init_colours():
+    lighter = []
+    darker = []
+    r = 128
+    g = 94
+    b = 64
+    # use top +- 64
+    # gives 128 in each domain
+    delta = 16
+    while r < 200:
+        g = 92
+        while g < 205:
+            b = 74
+            while b < 200:
+                lighter.append('{},{},{}'.format(r, g, b))
+                darker.append('{},{},{}'.format(r - 16, g - 16, b - 16))
+                b += delta
+            g += delta
+        r += delta
+
+    lighter.reverse()
+    darker.reverse()
+
+    print len(lighter)
+
+    return [lighter, darker]
+
 def get_cpu_tenancy_count_by_tid():
     pid_map = dict()
     max_sample_count = 0
@@ -46,6 +73,8 @@ def get_cpu_tenancy_count_by_tid():
 def write_svg_header(writer, width, height):
     writer.write('<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
     writer.write('<svg version="1.1" width="' + str(width) + '" height="' + str(height) + '"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n')
+    writer.write(
+        '<text text-anchor="middle" x="{}" y="30" font-size="20" font-family="monospace" fill="#000">Thread CPU tenancy</text>'.format(width / 2))
 
 def write_svg_footer(writer):
     writer.write('</svg>\n')
@@ -65,11 +94,13 @@ def overlay_thread_name(writer, x_offset, y_offset, thread_name):
     writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" style="text-overflow: clip" fill="#000">{}</text>'.format(x_offset, y_offset + 12, 1000, thread_name))
 
 
-def write_cell(writer, x_offset, y_offset, width, height, cpu_id, tid):
+def write_cell(writer, x_offset, y_offset, width, height, cpu_id, tid, percentage, colours):
     thread_name = str(tid)
-    cell_text = '{}/CPU{}'.format(thread_name, cpu_id)
+    cell_text = '{}/CPU{} ({:.2f}%)'.format(thread_name, cpu_id, percentage)
     writer.write('<g><title>{}</title>'.format(cell_text))
-    writer.write('<rect x="{}" y="{}" width="{}" height="{}" fill="{}">'.format(x_offset, y_offset, width, height, get_fill(cpu_id)))
+    fill = colours[0][(cpu_id * 8) % len(colours[0])]
+    stroke = colours[1][(cpu_id * 8) % len(colours[1])]
+    writer.write('<rect x="{}" y="{}" width="{}" height="{}" fill="rgb({})" stroke="rgb({})">'.format(x_offset, y_offset, width, height, fill, stroke))
     writer.write('</rect>\n')
     writer.write('<text x="{}" y="{}" width="{}" font-size="12" font-family="monospace" style="text-overflow: clip" fill="#000">{}</text>'.format(x_offset, y_offset + 22, width, "CPU{}".format(cpu_id)))
     writer.write('</g>\n')
@@ -80,6 +111,8 @@ def write_svg(width, height, cpu_tenancy_by_pid, max_sample_count, tid_to_thread
 
     row_height = float((height - 60) / calculate_number_of_columns(cpu_tenancy_by_pid))
     unit_width = float((width - 40) / float(max_sample_count))
+
+    colours = init_colours()
 
     y_offset = 50
     thread_name_to_tid = dict()
@@ -94,10 +127,10 @@ def write_svg(width, height, cpu_tenancy_by_pid, max_sample_count, tid_to_thread
                 thread_name = tid_to_thread_name[tid]
             for cpu_id in sorted(cpu_sample_count.iterkeys()):
                 if cpu_id is not 'all':
+                    percentage_of_total = 100 * float(cpu_sample_count[cpu_id] / float(cpu_sample_count['all']))
                     sample_width = unit_width * cpu_sample_count[cpu_id]
 
-                    # TODO add percentage to cell text
-                    write_cell(writer, x_offset, y_offset, sample_width, row_height, cpu_id, thread_name)
+                    write_cell(writer, x_offset, y_offset, sample_width, row_height, cpu_id, thread_name, percentage_of_total, colours)
                     x_offset += sample_width
             overlay_thread_name(writer, 20, y_offset, thread_name)
             y_offset += row_height
