1a88b140ad77ce4a2df4bef6941a2f6cda5b36ec
epickrram <mark@epickrram.com>
2017-08-02 09:00:58 +0100

    print stacks

--- src/heap/heap_profile.py
+++ src/heap/heap_profile.py
@@ -19,28 +19,29 @@ prog="""
 
 struct key_t {
     u32 pid;
-    u64 kernel_ip;
-    u64 kernel_ret_ip;
     int user_stack_id;
-    int kernel_stack_id;
-    char name[TASK_COMM_LEN];
+    char name[256];
 };
 BPF_HASH(counts, struct key_t);
 BPF_HASH(start, u32);
+BPF_HASH(tids, long);
 BPF_STACK_TRACE(stack_traces, 10240)
 
-int trace_alloc(void *ctx) {
+int trace_alloc(struct pt_regs *ctx, long tid, char *name, int nameLength, int wordSize) {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
+    // not really necessary, as USDT attaches to pid
     if (!(pid == %s))
         return 0;
 
-    // create map key
-    u64 zero = 0, *val;
+
+    u64 zero = 0, *val, *tc;
     struct key_t key = {.pid = pid};
-    bpf_get_current_comm(&key.name, sizeof(key.name));
+    bpf_probe_read(&key.name, sizeof(key.name), (void *)PT_REGS_PARM3(ctx));
+
+    tc = tids.lookup_or_init(&tid, &zero);
+    (*tc)++;
 
     key.user_stack_id = stack_traces.get_stackid(ctx, 0 | BPF_F_REUSE_STACKID | BPF_F_USER_STACK);
-    key.kernel_stack_id = -1;
 
     val = counts.lookup_or_init(&key, &zero);
     (*val)++;
@@ -52,18 +53,38 @@ int trace_alloc(void *ctx) {
 
 # single arg is an oop - need header file from jdk
 # http://hg.openjdk.java.net/jdk7u/jdk7u/hotspot/file/f49c3e79b676/src/share/vm/oops/oop.hpp
-
+pid=int(sys.argv[1])
 usdt = USDT(path="/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so", pid=int(sys.argv[1]))
 usdt.enable_probe(probe="object__alloc", fn_name="trace_alloc")
 bpf = BPF(text=prog % (int(sys.argv[1])), usdt_contexts=[usdt])
 
 
 time.sleep(5)
+
+if len(bpf["tids"]) == 0:
+    print "No data found - are DTrace probes enabled in running process?"
+
 stack_traces = bpf["stack_traces"]
+all_stacks=[]
+stack_counts=dict()
 for k, v in bpf["counts"].iteritems():
-    print "%d allocations at" % v.value
+    stack=[]
     for addr in stack_traces.walk(k.user_stack_id):
         pid = k.pid
-        print bpf.sym(addr, pid) + (" (from raw %-16x " % addr) + ")"
+        symbol=bpf.sym(addr, pid)
+        if symbol == "[unknown]":
+            stack.append(("0x%-16x" % addr).strip())
+        else:
+            stack.append(symbol.strip())
+    stack.reverse()
+    try:
+        stack_counts[k.name.strip().encode('utf-8', errors='replace') + ";" + ";".join(stack).encode('utf-8', errors='replace')] = int(v.value)
+    except UnicodeDecodeError:
+        print "Failed to decode stack: " + str(stack)
+
+
+
+for k, v in stack_counts.iteritems():
+    print k + " " + str(v)
 
 
