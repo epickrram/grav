3aa9dcd8d2140b9efdf888f308efce44778bbcd8
alanger <alanger@ebay.com>
2017-12-20 16:42:54 +0200

    fix vagrant script by using 'uname -r' for (the right) linux tools version

--- vagrant-grav/install-perf.sh
+++ vagrant-grav/install-perf.sh
@@ -2,9 +2,8 @@
 
 apt-get update
 apt-get --assume-yes install linux-tools-common 
-apt-get --assume-yes install linux-tools-generic linux-cloud-tools-generic
-apt-get --assume-yes install linux-tools-4.10.0-28-generic linux-cloud-tools-4.10.0-28-generic
-#apt-get --assume-yes install linux-tools-4.4.0-81-generic linux-cloud-tools-4.4.0-81-generic
+apt-get --assume-yes install linux-tools-generic linux-cloud-tools-generic 
+apt-get --assume-yes  install linux-tools-`uname -r`
 apt-get --assume-yes install build-essential
 
 perf list
